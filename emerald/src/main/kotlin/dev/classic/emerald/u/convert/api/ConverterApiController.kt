ace6fbc97ff4598cf134a5574da8ade605afc2fe
Danilo3 <doka-97@yandex.ru>
2023-06-01 22:56:14 +0300

    [code]: just better stubs

--- emerald/src/main/kotlin/dev/classic/emerald/u/convert/api/ConverterApiController.kt
+++ emerald/src/main/kotlin/dev/classic/emerald/u/convert/api/ConverterApiController.kt
@@ -2,18 +2,25 @@ package dev.classic.emerald.u.convert.api
 
 import dev.classic.emerald.u.convert.api.post.PostConvertRequestBody
 import dev.classic.emerald.u.convert.api.post.PostConvertResponseBody
+import dev.classic.emerald.u.convert.service.ConverterService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
-class ConverterApiController {
+class ConverterApiController(
+    private val service: ConverterService) {
 
     @PostMapping("/convert")
     fun convert(@RequestBody body: PostConvertRequestBody) : Mono<PostConvertResponseBody>
     {
-       // return Mono.just(PostConvertResponseBody())
+        return service.convert()
+            .map(::toResponse)
+    }
+
+    private fun toResponse(any: Any?): PostConvertResponseBody? {
+        TODO("Not yet implemented")
     }
 
 
--- emerald/src/main/kotlin/dev/classic/emerald/u/convert/service/ConverterService.kt
+++ emerald/src/main/kotlin/dev/classic/emerald/u/convert/service/ConverterService.kt
@@ -1,4 +1,13 @@
 package dev.classic.emerald.u.convert.service
 
+import dev.classic.emerald.u.convert.core.ConvertResult
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+@Service
 class ConverterService {
+
+    fun convert() : Mono<in ConvertResult>{
+        return Mono.just()
+    }
 }
